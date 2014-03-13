f12685e93eefb5f3ebbc260f4656b407a73d564b
Tony Gregerson <tony.gregerson@gmail.com>
2014-03-13 12:44:42 -0500

    Add TCLAP for command-line flag support

--- Graphs/lp_solve_interface_main.cpp
+++ Graphs/lp_solve_interface_main.cpp
@@ -2,14 +2,25 @@
 
 #include <cstdlib>
 
+#include "tclap/CmdLine.h"
+
 using namespace std;
 
 void print_usage_and_exit() {
-  cout << "Usage: lp_solve_interface timeout_s "
+  cout << "Usage: lp_solve_interface INPUT_ARGS [ADDITIONAL_OPTIONS*]" << endl
+       << endl
+       << "INPUT_ARGS: " << endl
        << "(-chaco chaco_graph_input_file] | "
        << "-ntl ntl_input_file | "
-       << "-mps mps_input_file) "
-       << "[-write_mps mps_output_file] "
+       << "-mps mps_input_file) " << endl
+       << endl
+       << "ADDITIONAL_OPTIONS:" << endl
+       << "-imbalance imbalance_float" << endl
+       << "-solve [SOLVE_OPTIONS*]" << endl
+       << "-write_mps mps_output_file" << endl
+       << endl
+       << "SOLVE_OPTIONS:" << endl
+       << "-timeout timeout_s "
        << endl;
   exit(1);
 }
@@ -20,36 +31,75 @@ int main(int argc, char *argv[]) {
     print_usage_and_exit();
   }
 
+  bool solve = false;
   bool use_chaco = false;
   bool use_ntl = false;
   bool use_mps = false;
   bool write_mps = false;
+  long timeout_s = 0;
 
-  long timeout_s = atol(argv[1]);
+  string input_filename;
+  string output_filename;
 
-  string input_type_str(argv[2]);
-  if (input_type_str == "-chaco") {
-    use_chaco = true;
-  } else if (input_type_str == "-ntl") {
-    use_ntl = true;
-  } else if (input_type_str == "-mps") {
-    use_mps = true;
-  } else {
-    print_usage_and_exit();
-  }
-  string input_filename(argv[3]);
+  // Parse command-line arguments
+  try {
+    TCLAP::CmdLine cmd("Command description message", ' ', '0.0');
 
-  string output_mps_filename;
-  if (argc > 4) {
-    if (argc != 6) {
-      print_usage_and_exit();
+    vector<TCLAP::Arg*> input_file_args;
+    TCLAP::ValueArg<string> chaco_input_file_flag(
+        "c", "chaco", "CHACO-format input file name", false, "string",
+        nullptr);
+    input_file_args.push_back(&chaco_input_file_flag);
+
+    TCLAP::ValueArg<string> ntl_input_file_flag(
+        "n", "ntl", "NTL-format input file name", false, "string",
+        nullptr);
+    input_file_args.push_back(&ntl_input_file_flag);
+
+    TCLAP::ValueArg<string> mps_input_file_flag(
+        "m", "mps", "MPS-format input file name", false, "string",
+        nullptr);
+    input_file_args.push_back(&mps_input_file_flag);
+
+    cmd.xorAdd(input_file_args);
+
+    TCLAP::ValueArg<string> mps_output_file_flag(
+        "o", "output_mps", "Output MPS file name", false, "string", cmd,
+        nullptr);
+
+    TCLAP::ValueArg<long> solver_timeout_flag(
+        "t", "timeout_s", "Solver timeout in seconds", false, "long", cmd,
+        nullptr);
+
+    TCLAP::SwitchArg solve_switch(
+        "s", "solve", "Solve LP model", cmd, false);
+
+    cmd.parse(argc, argv);
+
+    if (chaco_input_file_flag.isSet()) {
+      use_chaco = true;
+      input_filename = chaco_input_file_flag.getValue();
+    } else if (ntl_input_file_flag.isSet()) {
+      use_ntl = true;
+      input_filename = ntl_input_file_flag.getValue();
+    } else if (mps_input_file_flag.isSet()) {
+      use_mps = true;
+      input_filename = mps_input_file_flag.getValue();
     }
-    string write_output_str(argv[4]);
-    if (write_output_str != "-write_mps") {
-      print_usage_and_exit();
+
+    if (mps_output_file_flag.isSet()) {
+      write_mps = true;
+      output_filename = mps_output_file_flag.getValue();
     }
-    write_mps = true;
-    output_mps_filename = argv[5];
+
+    solve = solve_switch.getValue();
+
+    if (solve && solver_timeout_flag.isSet()) {
+      timeout_s = solver_timeout_flag.getValue();
+    }
+
+  } catch (TCLAP::ArgException &e) {
+    cerr << "Error: " << e.error() << " for arg " << e.argId() << endl;
   }
 
   LpSolveInterface interface;
@@ -61,10 +111,12 @@ int main(int argc, char *argv[]) {
     interface.LoadFromMps(input_filename);
   }
 
-  interface.RunSolver(timeout_s);
+  if (solve) {
+    interface.RunSolver(timeout_s);
+  }
 
   if (write_mps) {
-    interface.WriteToMps(output_mps_filename);
+    interface.WriteToMps(output_filename);
   }
 
   return 0;
